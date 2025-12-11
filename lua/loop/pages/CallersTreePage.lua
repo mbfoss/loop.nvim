0f5f5e261b3671d534e07eeefc5a51b65cec4a38
Dev <Dev@Ms-MacBook-Air.local>
2025-12-11 20:15:58 +0100

    update

--- lua/loop/pages/CallersTreePage.lua
+++ lua/loop/pages/CallersTreePage.lua
@@ -36,54 +36,84 @@ function CallersTreePage:_add_keymaps()
     })
 end
 
-function CallersTreePage:_load_callers(item, callback)
-    -- item must always be a CallHierarchyItem
-    vim.lsp.buf_request(0, "callHierarchy/incomingCalls", { item = item }, function(err, calls)
-        if err or not calls then
+function CallersTreePage:_load_callers(win_id, item, callback, visited)
+    visited = visited or {}
+
+    -- prevent cycles using the opaque data token
+    local key = item.data
+    if visited[key] then
+        callback({})
+        return
+    end
+    visited[key]              = true
+
+    -- convert LSP range to position for prepareCallHierarchy
+    local bufnr               = vim.uri_to_bufnr(item.uri)
+    local line                = item.selectionRange.start.line
+    local col                 = item.selectionRange.start.character
+    local params              = vim.lsp.util.make_position_params(win_id, "utf-8")
+    params.position.line      = line
+    params.position.character = col
+
+    -- call prepareCallHierarchy fresh for this symbol
+    vim.lsp.buf_request(bufnr, "textDocument/prepareCallHierarchy", params, function(err, items)
+        if err or not items or #items == 0 then
             callback({})
             return
         end
 
-        local children = {}
+        local root_item = items[1] -- fresh LSP CallHierarchyItem
 
-        for _, call in ipairs(calls) do
-            local from = call.from -- CallHierarchyItem
-            local filename = vim.uri_to_fname(from.uri)
-            local lnum = from.range.start.line + 1
-            local col = from.range.start.character
-
-            local child_item_id = string.format("%s:%d:%d",
-                from.uri, from.range.start.line, from.range.start.character)
-
-            local node = {
-                id = child_item_id,
-                expanded = false,
-                data = {
-                    name = from.name or "<anonymous>",
-                    filename = filename,
-                    lnum = lnum,
-                    col = col,
-                }
-            }
-
-            node.children_callback = function(cb)
-                -- Recursive call with the *hierarchy item*, not a position
-                self:_load_callers(from, cb)
+        -- now fetch incoming calls
+        vim.lsp.buf_request(bufnr, "callHierarchy/incomingCalls", { item = root_item }, function(err2, calls)
+            if err2 or not calls then
+                callback({})
+                return
             end
 
-            table.insert(children, node)
-        end
+            local children = {}
 
-        callback(children)
+            for _, call in ipairs(calls) do
+                local from = call.from -- original LSP item
+
+                if call.fromRanges and call.fromRanges[1] then
+                    from.originSelectionRange = call.fromRanges[1]
+                end
+
+                local filename = vim.uri_to_fname(from.uri)
+                local lnum     = from.range.start.line + 1
+                local col      = from.range.start.character
+
+                local node     = {
+                    id = tostring({}),
+                    expanded = false,
+                    data = {
+                        name = from.name or "<anonymous>",
+                        filename = filename,
+                        lnum = lnum,
+                        col = col,
+                    },
+                    children_callback = function(cb)
+                        -- recurse by preparing hierarchy again
+                        self:_load_callers(win_id, from, cb, visited)
+                    end,
+                }
+
+                table.insert(children, node)
+            end
+
+            callback(children)
+        end)
     end)
 end
 
-function CallersTreePage:load(cursor_pos)
+function CallersTreePage:load()
     self:clear_items()
 
-    local bufnr = vim.api.nvim_get_current_buf()
-    local symbol_position = vim.lsp.util.make_position_params(0, "utf-8")
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local bufnr           = vim.api.nvim_get_current_buf()
+    local win_id          = vim.api.nvim_get_current_win()
+    local line, col       = unpack(vim.api.nvim_win_get_cursor(win_id))
+    local symbol_position = vim.lsp.util.make_position_params(win_id, "utf-8")
 
     vim.lsp.buf_request(bufnr, "textDocument/prepareCallHierarchy", symbol_position, function(err, items)
         if err or not items or #items == 0 then
@@ -101,14 +131,14 @@ function CallersTreePage:load(cursor_pos)
             expanded = true,
             data = {
                 name = target.name or "<symbol>",
-                filename = vim.api.nvim_buf_get_name(0),
+                filename = vim.api.nvim_buf_get_name(bufnr),
                 lnum = line,
                 col = col,
             },
         }
 
         root.children_callback = function(cb)
-            self:_load_callers(target, cb)
+            self:_load_callers(win_id, target, cb)
         end
 
         self:upsert_item(root)
